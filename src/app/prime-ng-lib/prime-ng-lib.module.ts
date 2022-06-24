b39e055946b3769b8328049b260ac3604464ba00
codelaghien <codelaghien.github.io>
2022-06-23 20:59:55 -0700

    master

--- src/app/prime-ng-lib/prime-ng-lib.module.ts
+++ src/app/prime-ng-lib/prime-ng-lib.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
+import { DataViewModule } from 'primeng/dataview';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, CheckboxModule, DropdownModule],
-  exports: [CheckboxModule, DropdownModule],
+  imports: [
+    CommonModule,
+    CheckboxModule,
+    DropdownModule,
+    DataViewModule,
+    TableModule,
+  ],
+  exports: [CheckboxModule, DropdownModule, DataViewModule, TableModule],
 })
 export class PrimeNgLibModule {}
